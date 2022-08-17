9faa3f513f4bb071512f03c5c165024d389bafe5
almasud <dev.almasud@gmail.com>
2022-08-17 12:34:25 +0600

    Type aliace and function.

--- output/script.js
+++ output/script.js
@@ -1,3 +1,9 @@
 "use strict";
-const country = "Bangladesh";
-console.log(`I love ${country}.`);
+const userDetails = (id, user) => {
+    console.log(`User id: ${id}, name: ${user.name} and age: ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr."}`);
+};
+userDetails(23, { name: "Almasud", age: 23 });
+sayHello({ name: "Rafik", age: 65 });
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,16 @@
-const country = "Bangladesh";
+type StringOrNumber = string | number;
+type UserType = {name: string, age: number}
 
-console.log(`I love ${country}.`)
+const userDetails = (
+    id: StringOrNumber,
+    user: UserType,
+) => {
+    console.log(`User id: ${id}, name: ${user.name} and age: ${user.age}`);
+}
+
+const sayHello = (user: UserType) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr."}`);
+}
+
+userDetails(23, {name: "Almasud", age: 23});
+sayHello({name: "Rafik", age: 65});
